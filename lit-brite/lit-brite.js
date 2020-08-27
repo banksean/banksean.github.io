95a0f2704a3445911609392aab9c5446c75edae5
Sean McCullough <banksean@gmail.com>
2020-08-27 00:53:25 -0700

    Update lit-brite.js

--- lit-brite/lit-brite.js
+++ lit-brite/lit-brite.js
@@ -4,6 +4,64 @@ import {
   css
 } from "https://unpkg.com/lit-element/lit-element.js?module";
 
+
+class RLE {
+  // given a string, returns a run length encoded string
+  static encode(str) {
+    let count = 0;
+    let char = '';
+    let out = '';
+
+    str.split('').forEach((alpha) => {
+      count += 1;
+      if (alpha === char) {
+        return;
+      }
+      if (char !== '') {
+        out += this.appendEncoded(char, count);
+      }
+      char = alpha;
+      count = 0;
+    });
+    out += this.appendEncoded(char, count + 1);
+
+    return out;
+  }
+
+  // given a run length encoded string, returns the decoded string
+  static decode(str) {
+    // use a string to store count to account for multi digit nums
+    let count = '';
+    let out = '';
+
+    str.split('').forEach((char) => {
+      // build the count string
+      if (char.match(/\d/)) {
+        count += char;
+        return;
+      }
+      // handle single char runs
+      count = count ? parseInt(count, 10) : 1;
+      // using the count and the current char, build the output string
+      for (let i = 0; i < count; i += 1) {
+        out += char;
+      }
+      count = '';
+    });
+
+    return out;
+  }
+
+  // helper method to build the encoded string
+  static appendEncoded(char, count) {
+    let result = '';
+
+    result += count > 1 ? count.toString() : '';
+
+    return result + char;
+  }
+}
+
 const numPegs = 12 * (30 + 31); // 24 alternating rows of 30 and 31 cols.
 const colors = [
   "blank",
@@ -246,60 +304,3 @@ class LitBritePeg extends LitElement {
 }
 
 customElements.define("lit-brite-peg", LitBritePeg);
-
-class RLE {
-  // given a string, returns a run length encoded string
-  static encode(str) {
-    let count = 0;
-    let char = '';
-    let out = '';
-
-    str.split('').forEach((alpha) => {
-      count += 1;
-      if (alpha === char) {
-        return;
-      }
-      if (char !== '') {
-        out += this.appendEncoded(char, count);
-      }
-      char = alpha;
-      count = 0;
-    });
-    out += this.appendEncoded(char, count + 1);
-
-    return out;
-  }
-
-  // given a run length encoded string, returns the decoded string
-  static decode(str) {
-    // use a string to store count to account for multi digit nums
-    let count = '';
-    let out = '';
-
-    str.split('').forEach((char) => {
-      // build the count string
-      if (char.match(/\d/)) {
-        count += char;
-        return;
-      }
-      // handle single char runs
-      count = count ? parseInt(count, 10) : 1;
-      // using the count and the current char, build the output string
-      for (let i = 0; i < count; i += 1) {
-        out += char;
-      }
-      count = '';
-    });
-
-    return out;
-  }
-
-  // helper method to build the encoded string
-  static appendEncoded(char, count) {
-    let result = '';
-
-    result += count > 1 ? count.toString() : '';
-
-    return result + char;
-  }
-}
