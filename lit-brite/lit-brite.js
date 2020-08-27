c493f4d0dcbe45b57bde30c53e3fc45962b4161d
Sean McCullough <banksean@gmail.com>
2020-08-27 00:47:49 -0700

    Update lit-brite.js

--- lit-brite/lit-brite.js
+++ lit-brite/lit-brite.js
@@ -47,7 +47,8 @@ class LitBrite extends LitElement {
     return ret;
   }
 
-  unpackPegs(pegString) {
+  unpackPegs(rleString) {
+    let pegString = RLE.decode(rleString);
     let ret = [];
     for (let i = 0; i < pegString.length; i++) {
       ret.push({ color: colors[Number(pegString[i])] });
@@ -57,7 +58,7 @@ class LitBrite extends LitElement {
 
   save() {
     let pegStr = this.packPegs(this.pegs);
-    history.replaceState({}, location.pathname, '?'+pegStr);
+    history.replaceState({}, location.pathname, '?'+RLE.encode(pegStr));
   }
 
   load() {
@@ -245,3 +246,60 @@ class LitBritePeg extends LitElement {
 }
 
 customElements.define("lit-brite-peg", LitBritePeg);
+
+class RLE {
+  // given a string, returns a run length encoded string
+  static encode(str) {
+    let count = 0;
+    let char = '';
+    let out = '';
+
+    str.split('').forEach((alpha) => {
+      count += 1;
+      if (alpha === char) {
+        return;
+      }
+      if (char !== '') {
+        out += this.appendEncoded(char, count);
+      }
+      char = alpha;
+      count = 0;
+    });
+    out += this.appendEncoded(char, count + 1);
+
+    return out;
+  }
+
+  // given a run length encoded string, returns the decoded string
+  static decode(str) {
+    // use a string to store count to account for multi digit nums
+    let count = '';
+    let out = '';
+
+    str.split('').forEach((char) => {
+      // build the count string
+      if (char.match(/\d/)) {
+        count += char;
+        return;
+      }
+      // handle single char runs
+      count = count ? parseInt(count, 10) : 1;
+      // using the count and the current char, build the output string
+      for (let i = 0; i < count; i += 1) {
+        out += char;
+      }
+      count = '';
+    });
+
+    return out;
+  }
+
+  // helper method to build the encoded string
+  static appendEncoded(char, count) {
+    let result = '';
+
+    result += count > 1 ? count.toString() : '';
+
+    return result + char;
+  }
+}
